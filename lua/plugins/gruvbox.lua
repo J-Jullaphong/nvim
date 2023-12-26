7aa96c1a777789d681e8fde2530e46f6bd68d91f
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2023-12-26 14:43:17 +0700

    Change from Gruvbox to Catppuccin

--- lua/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ellisonleao/gruvbox.nvim",
-  opts = ...,
-  config = function()
-    vim.o.background = "dark" -- or "light" for light mode
-    vim.cmd([[colorscheme gruvbox]])
-  end
-}
