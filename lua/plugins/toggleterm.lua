bce1c92b484f39a1ab12bf5924913e74e013ece9
Jullaphong JIAMWATTHANALOET <jullaphong03@gmail.com>
2024-01-02 22:32:05 +0700

    Add Lazygit Integration to ToggleTerm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,20 @@
 return {
-  'akinsho/toggleterm.nvim', 
+  'akinsho/toggleterm.nvim',
   version = "*",
-  config = true,
   config = function()
     require("toggleterm").setup{}
     vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>', {})
     vim.keymap.set('n', '<leader>th', ':ToggleTerm direction=horizontal<CR>', {})
     vim.keymap.set('n', '<leader>tv', ':ToggleTerm direction=vertical<CR>', {})
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", direction="float", hidden = true })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.keymap.set("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
   end
 }
